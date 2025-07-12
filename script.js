f053a4e530531d51f0d5d946b79c95695bbb4d76
hammuod <151031260+hammuod@users.noreply.github.com>
2025-07-12 21:56:21 +0300

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+let lightMode = localStorage.getItem('lightMode'); 
+const themeSwitch = document.getElementById('modes');
+
+const enableLightMode = () => {
+    document.body.classList.add('light-Mode');
+    localStorage.setItem('lightMode', 'active');
+};
+
+const disableLightMode = () => {
+    document.body.classList.remove('light-Mode');
+    localStorage.setItem('lightMode', null);
+};
+
+if (lightMode === 'active') {
+    enableLightMode();
+} else {
+    disableLightMode();
+}
+
+themeSwitch.addEventListener("click", () => {
+    lightMode = localStorage.getItem('lightMode');
+    if (lightMode !== 'active') {
+        enableLightMode();
+    } else {
+        disableLightMode();
+    }
+});
+
